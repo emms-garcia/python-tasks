87b1815accf67d26048b7b0f42e88ef1c6c6918d
Emmanuel Garcia <emmanuel.garcia.solis@gmail.com>
2018-07-20 13:06:10 -0500

    Add template code.

---
+++ clock.py
@@ -0,0 +1,11 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+
+@sched.scheduled_job('interval', minutes=3)
+def timed_job():
+    print('This job runs every three minutes.')
+
+
+sched.start()
