0d06e224c0f09424ad85b9a09dbd1a0f8f73f1e6
Emmanuel Garcia <emmanuel.garcia.solis@gmail.com>
2018-07-20 15:14:31 -0500

    Add redis...

--- worker.py
+++ worker.py
@@ -6,7 +6,7 @@ from huey import RedisHuey, crontab
 huey = RedisHuey('python-tasks', host=os.environ.get('REDIS_URL'))
 
 
-@huey.periodic_task(crontab(minute='*/3'))
-def every_3_minutes():
-    print('every_3_minutes')
+@huey.periodic_task(crontab(minute='*/1'))
+def every_minute():
+    print('every_minute')
     print(datetime.utcnow())
